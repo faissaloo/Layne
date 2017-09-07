3fc34dc797ffb669b18dd61b3fcbd7aa50339e89
faissaloo <faissaloo@gmail.com>
2017-09-07 09:52:20 +0100

    Fixed last methods were not being bound to the object

--- lib/objects/dyn_objs.c
+++ lib/objects/dyn_objs.c
@@ -153,7 +153,7 @@ struct dyn_obj* get_member(struct dyn_obj *obj,char member_name[])
 
 void init_methods(struct dyn_obj *self, const struct method_list *methods)
 {
-	for (iter_t i=0;i<methods->count;i++)
+	for (iter_t i=0;i<=methods->count;i++)
 	{
 		bind_method(self,methods->method_pair[i].method_name,methods->method_pair[i].method);;
 	}
