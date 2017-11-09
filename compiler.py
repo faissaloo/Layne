33c83eaa7e65183c3e909cc7802edeee19927305
faissaloo <faissaloo@gmail.com>
2017-11-09 18:58:09 +0000

    Made some formatting mistakes for errors

--- compiler.py
+++ compiler.py
@@ -1155,10 +1155,8 @@ def parseLayer(tokens,noAssign=False):
 					tree.tree.pop(tree.cursor+1)
 					tree.tree.pop(tree.cursor+1)
 				elif i[1]=="for":
-
-
 					if tree.cursor+1>=len(tree):
-						print("\033[91mFatal error:\033[m For loop has no iterable, iterator or action on line"+str(tree[tree.cursor][3])+"\n"+
+						print("\033[91mFatal error:\033[m For loop has no iterable, iterator or action on line "+str(tree[tree.cursor][3])+"\n"+
 							txt.split("\n")[tree[tree.cursor][3]]+"\n"+
 							(" "*(tree[tree.cursor][2]))+"^",file=sys.stderr)
 						exit(255)
